a2599a0d3b8482cd6bd40a665143af4f8bb4bc6d
Sergey Zhirkov <zhirkov_55250@mail.ru>
2020-09-22 01:51:14 +0500

    complete Stage #3: Enough coffee for everyone

--- Coffee Machine (1) (1)/task/src/machine/CoffeeMachine.java
+++ Coffee Machine (1) (1)/task/src/machine/CoffeeMachine.java
@@ -4,13 +4,30 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Write how many cups of coffee you will need: ");
+
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int water = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        int coffeeBeans = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need: ");
         int cupsCount = scanner.nextInt();
-        System.out.printf("For %d cups of coffee you will need:\n", cupsCount);
-        System.out.println(String.format("%d ml of water\n" +
-                "%d ml of milk\n" +
-                "%d g of coffee beans", 200*cupsCount, 50*cupsCount, 15*cupsCount));
+
+        water /= 200;
+        milk /= 50;
+        coffeeBeans /= 15;
+        int maxCupsCount = Math.min(Math.min(water, milk), coffeeBeans);
+
+        if (maxCupsCount < cupsCount) {
+            System.out.printf("No, I can make only %d cup(s) of coffee", maxCupsCount);
+        } else {
+            System.out.print("Yes, I can make that amount of coffee ");
+            if (maxCupsCount > cupsCount) {
+                System.out.printf("(and even %d more than that)", maxCupsCount - cupsCount);
+            }
+        }
 
     }
 }
